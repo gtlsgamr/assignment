e20b3236f06728730fada4630038af12e4dca210
Hitarth Thummar <hittarth91@gmail.com>
2024-04-05 23:20:18 +0530

    Fix

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 This is my submission for the CricHeroes Practical Test- Full Stack Developer Semi Final Match.
 
 # How to run the code
+
 1. Clone the repository
 2. Run the following command to install the dependencies
+
 ```bash
 npm install
 ```
+
 3. Run the following command to run the code
+
 ```bash
 npm start
 ```
+
 4. Open the browser and go to the following URL
+
 ```bash
 http://localhost:3000
 ```
@@ -20,9 +26,11 @@ Below is the problem statement for the test.
 ---
 
 # CricHeroes Practical Test- Full Stack Developer
+
 ## Semi Final Match
 
 Congratulations on completing the league round of the tournament! Here you go with your Semi Final match. You have just one match before reaching the final. Some match-winning tips:
+
 - Use Node JS / Python
 - Prevent invalid input.
 - Follow proper naming conventions.
@@ -30,52 +38,57 @@ Congratulations on completing the league round of the tournament! Here you go wi
 - Do not use databases.
 
 ### What we are looking for
+
 - How is it like to collaborate and work with you? (It is okay to ask questions anytime to clear yourself.)
 - How passionate are you to get an accurate result? (Do not worry about rework but aim for accuracy.)
 - Do not hurry to complete the assignment. (You can take the time you want.)
 
 ### Problem Definition
+
 Here is an example points table of the IPL 2022 tournament.
 
-| # | Team                    | Matches | Won | Lost | NRR    | For        | Against    | Pts |
-|---|-------------------------|---------|-----|------|--------|------------|------------|-----|
-| 1 | Chennai Super Kings     | 7       | 5   | 2    | 0.771  | 1130/133.1 | 1071/138.5 | 10  |
-| 2 | Royal Challengers Bangalore | 7    | 4   | 3    | 0.597  | 1217/140   | 1066/131.4 | 8   |
-| 3 | Delhi Capitals          | 7       | 4   | 3    | 0.319  | 1085/126   | 1136/137   | 8   |
-| 4 | Rajasthan Royals        | 7       | 3   | 4    | 0.331  | 1066/128.2 | 1094/137.1 | 6   |
-| 5 | Mumbai Indians          | 8       | 2   | 6    | -1.75  | 1003/155.2 | 1134/138.1 | 4   |
+| #   | Team                        | Matches | Won | Lost | NRR   | For        | Against    | Pts |
+| --- | --------------------------- | ------- | --- | ---- | ----- | ---------- | ---------- | --- |
+| 1   | Chennai Super Kings         | 7       | 5   | 2    | 0.771 | 1130/133.1 | 1071/138.5 | 10  |
+| 2   | Royal Challengers Bangalore | 7       | 4   | 3    | 0.597 | 1217/140   | 1066/131.4 | 8   |
+| 3   | Delhi Capitals              | 7       | 4   | 3    | 0.319 | 1085/126   | 1136/137   | 8   |
+| 4   | Rajasthan Royals            | 7       | 3   | 4    | 0.331 | 1066/128.2 | 1094/137.1 | 6   |
+| 5   | Mumbai Indians              | 8       | 2   | 6    | -1.75 | 1003/155.2 | 1134/138.1 | 4   |
 
 You need to prepare a tool which will help any team reach a desired position in the points table.
 
 ### User Inputs
-1) Your Team
-2) Opposition Team
-3) How many overs match?
-4) Desired Position for Your Team in Points Table
-5) Toss Result: Batting First / Bowling First
-6) Runs Scored (Batting First) - Runs to Chase (Bowling First)
+
+1. Your Team
+2. Opposition Team
+3. How many overs match?
+4. Desired Position for Your Team in Points Table
+5. Toss Result: Batting First / Bowling First
+6. Runs Scored (Batting First) - Runs to Chase (Bowling First)
 
 ### Calculate NRR For Following Cases:
-1) Rajasthan Royals want to reach 3rd Position and their next match is against Delhi Capitals.
+
+1. Rajasthan Royals want to reach 3rd Position and their next match is against Delhi Capitals.
    a. If Rajasthan Royals bat first and score 120 runs in 20 overs then Rajasthan Royals should restrict Delhi Capitals for how many runs?
    b. If Delhi Capitals bat first and score 119 runs in 20 overs then Rajasthan Royals should chase 119 runs in how many overs?
 
-2) Rajasthan Royals want to reach 3rd Position and their next match is against Royal Challengers Bangalore.
+2. Rajasthan Royals want to reach 3rd Position and their next match is against Royal Challengers Bangalore.
    c. If Rajasthan Royals bat first and score 80 runs in 20 overs then Rajasthan Royals should restrict Royal Challengers Bangalore for how many runs? (hint: it should be a range)
    d. If Royal Challengers Bangalore bat first and score 79 runs in 20 overs then Rajasthan Royals should chase 79 runs in how many overs? (hint: it should be a range)
 
 ### Output Format
+
 - Q-1a: Answer
-    - If Rajasthan Royals score X_runs runs in X_overs overs, Rajasthan Royals need to restrict Delhi Capitals between X_runs to X_runs runs in X_overs.
-    - Revised NRR of Rajasthan Royals will be between X_nrr to X_nrr.
+  - If Rajasthan Royals score X_runs runs in X_overs overs, Rajasthan Royals need to restrict Delhi Capitals between X_runs to X_runs runs in X_overs.
+  - Revised NRR of Rajasthan Royals will be between X_nrr to X_nrr.
 - Q-1b: Answer
-    - Rajasthan Royals need to chase X_runs between X_overs and X_overs Overs.
-    - Revised NRR for Rajasthan Royals will be between X_nrr to X_nrr.
+  - Rajasthan Royals need to chase X_runs between X_overs and X_overs Overs.
+  - Revised NRR for Rajasthan Royals will be between X_nrr to X_nrr.
 - Q-2c: Answer,
-    - If X_team_name scores X_runs runs in X_overs overs, X_team_name needs to restrict X_team_name between X_runs to X_runs runs in X_overs.
-    - Revised NRR of X_team_name will be between X_nrr to X_nrr.
+  - If X_team_name scores X_runs runs in X_overs overs, X_team_name needs to restrict X_team_name between X_runs to X_runs runs in X_overs.
+  - Revised NRR of X_team_name will be between X_nrr to X_nrr.
 - Q-2d: Answer,
-    - X_team_name needs to chase X_runs between X_overs and X_overs Overs.
-    - Revised NRR for X_team_name will be between X_nrr to X_nrr.
+  - X_team_name needs to chase X_runs between X_overs and X_overs Overs.
+  - Revised NRR for X_team_name will be between X_nrr to X_nrr.
 
 All the best!!!
--- utils.js
+++ utils.js
@@ -1,140 +1,182 @@
-const points = require('./points_table');
+const points = require("./points_table");
 
 const convertOversToActualOvers = (overs) => {
-    // One over is equal to 6 balls
-    // e.g. If the overs are 1.4, then the actual overs are 1 + (4/6) = 1.667
-    return parseFloat((Math.floor(overs) + ((overs % 1) * 10) / 6).toFixed(3));
-}
+    return parseFloat(
+      (Math.floor(overs) + ((overs % 1) * 10) / 6).toFixed(3),
+  );
+};
 
-const convertRunsToOvers = (runs) => {
-    // One over is equal to 6 balls
-    // e.g. If the runs are 1.667, then the actual overs are 1 + (4/6) = 1.4
-    return (Math.floor(runs/6) + "." + ((runs%6)));
-}
+const convertBallsToOvers = (balls) => {
+    return convertOversToActualOvers(
+      parseFloat(Math.floor(balls / 6) + "." + (balls % 6)),
+  );
+};
 
 const calculateNrr = (forRuns, forOvers, againstRuns, againstOvers) => {
-    // NRR = (Runs scored / Overs faced) - (Runs conceded / Overs bowled)
-    const nrr = (forRuns / convertOversToActualOvers(forOvers)) - (againstRuns / convertOversToActualOvers(againstOvers));
-    return parseFloat(nrr.toFixed(3));
-}
+  // NRR = (Runs scored / Overs faced) - (Runs conceded / Overs bowled)
+  const nrr =
+    forRuns / convertOversToActualOvers(forOvers) -
+    againstRuns / convertOversToActualOvers(againstOvers);
+  return parseFloat(nrr.toFixed(3));
+};
 
 // Helper function to generate a range of numbers
-let range = (start, end) => [...Array(end - start + 1).keys()].map(i => i + start);
+let range = (start, end) =>
+  [...Array(end - start + 1).keys()].map((i) => i + start);
 
-const calculatePoints = (team1, team2, oversPlayed, position, tossResult, runsScored) => {
-    const team1Name = points[team1 - 1].Name;
-    const team2Name = points[team2 - 1].Name;
+const calculatePoints = (
+  team1,
+  team2,
+  oversPlayed,
+  position,
+  tossResult,
+  runsScored,
+) => {
+  const team1Name = points[team1 - 1].Name;
+  const team2Name = points[team2 - 1].Name;
 
-    if (position > points.length) {
-        return "You have selected a position that is not possible in the points table";
-    }
+  if (position > points.length) {
+    return "You have selected a position that is not possible in the points table";
+  }
 
-    if (team1 === team2) {
-        return "You cannot play against yourself";
-    }
+  if (team1 === team2) {
+    return "You cannot play against yourself";
+  }
 
-    if (team1 > points.length || team2 > points.length) {
-        return "You have selected a team that is not in the points table";
-    }
+  if (team1 > points.length || team2 > points.length) {
+    return "You have selected a team that is not in the points table";
+  }
 
-    if (!tossResult in ['Batting First', 'Bowling First']) {
-        return "Invalid toss result";
-    }
+  if ((!tossResult) in ["Batting First", "Bowling First"]) {
+    return "Invalid toss result";
+  }
 
-    if (runsScored < 0) {
-        return "Negative runs are not possible in cricket.";
-    }
+  if (runsScored < 0) {
+    return "Negative runs are not possible in cricket.";
+  }
 
-    if (points[team1 - 1].Pts + 2 < points[team2 - 1].Pts) {
-        return `${team1Name} cannot reach position ${position} by the result of this match`;
-    }
+  if (points[team1 - 1].Pts + 2 < points[team2 - 1].Pts) {
+    return `${team1Name} cannot reach position ${position} by the result of this match`;
+  }
 
-    if (points[team1 - 1].Pts + 2 > points[team2 - 1].Pts) {
-        return `${team1Name} can reach position ${position} by winning this match by any margin`;
-    }
+  if (points[team1 - 1].Pts + 2 > points[team2 - 1].Pts) {
+    return `${team1Name} can reach position ${position} by winning this match by any margin`;
+  }
 
-    if (points[team1 - 1].Pts + 2 === points[team2 - 1].Pts) {
-        const team1Runs = parseFloat(points[team1 - 1].For.split('/')[0]);
-        const team2Runs = parseFloat(points[team2 - 1].For.split('/')[0]);
-        const team1Overs = parseFloat(points[team1 - 1].For.split('/')[1]);
-        const team2Overs = parseFloat(points[team2 - 1].For.split('/')[1]);
-        const team1AgainstRuns = parseFloat(points[team1 - 1].Against.split('/')[0]);
-        const team2AgainstRuns = parseFloat(points[team2 - 1].Against.split('/')[0]);
-        const team1AgainstOvers = parseFloat(points[team1 - 1].Against.split('/')[1]);
-        const team2AgainstOvers = parseFloat(points[team2 - 1].Against.split('/')[1]);
-        const targetNrr = parseFloat(points[position - 1].NRR);
-        const targetName = points[position - 1].Name;
-        const aboveTargetNrr = parseFloat(points[position - 2].NRR);
+  if (points[team1 - 1].Pts + 2 === points[team2 - 1].Pts) {
+    const team1Runs = parseFloat(points[team1 - 1].For.split("/")[0]);
+    const team2Runs = parseFloat(points[team2 - 1].For.split("/")[0]);
+    const team1Overs = parseFloat(points[team1 - 1].For.split("/")[1]);
+    const team2Overs = parseFloat(points[team2 - 1].For.split("/")[1]);
+    const team1AgainstRuns = parseFloat(
+      points[team1 - 1].Against.split("/")[0],
+    );
+    const team2AgainstRuns = parseFloat(
+      points[team2 - 1].Against.split("/")[0],
+    );
+    const team1AgainstOvers = parseFloat(
+      points[team1 - 1].Against.split("/")[1],
+    );
+    const team2AgainstOvers = parseFloat(
+      points[team2 - 1].Against.split("/")[1],
+    );
+    const targetNrr = parseFloat(points[position - 1].NRR);
+    const targetName = points[position - 1].Name;
+    const aboveTargetNrr = parseFloat(points[position - 2].NRR);
 
-        if (tossResult === "Batting First") {
-            const result = []
-            const team2Position = points[team2 - 1].id
-            const team1Position = points[team1 - 1].id
-            // Loop through all the possible runs that can be scored
-            // by the batting team in the given overs played
-            // and calculate the NRR for both teams
-            // NOTE: This can be possibly optimized by two pointer search, but I have not implemented it here.
-            range(1, runsScored - 1).forEach(runs => {
-                const team1Nrr = parseFloat((calculateNrr(
-                    team1Runs + runsScored,
-                    team1Overs + oversPlayed,
-                    team1AgainstRuns + runs,
-                    team1AgainstOvers + oversPlayed
-                )).toFixed(3));
-                const team2Nrr = parseFloat((calculateNrr(
-                    team2Runs + runs,
-                    team2Overs + oversPlayed,
-                    team2AgainstRuns + runsScored,
-                    team2AgainstOvers + oversPlayed
-                )).toFixed(3));
-                if (
-                    ( team2Position === position  && team1Nrr < aboveTargetNrr && team1Nrr > team2Nrr) ||
-                    (team2Position > position && team1Nrr > targetNrr && team1Nrr < aboveTargetNrr)         ||
-                    (team2Position < position && team1Nrr > targetNrr && team1Nrr < aboveTargetNrr && team1Nrr < team2Nrr)// Check if the NRR is between the target and the one above the target
-                ) {
-
-                    result.push([runs, team1Nrr, team2Nrr])
-                }
-            })
-            if (result.length === 0) {
-                return `${team1Name} cannot reach position ${position} by the result of this match`;
-            } else {
-                return `If ${team1Name} scores ${runsScored} runs in ${oversPlayed} overs, ${team1Name} needs to restrict ${team2Name} between ${result[0][0]} and ${result[result.length -1][0]} runs in ${oversPlayed} overs. \nThe revised NRR of ${team1Name} will be between ${result[result.length -1][1]} and ${result[0][1]}.`
-            }
-
-        } else if (tossResult === "Bowling First") {
-            const result = []
-            const team2Position = points[team2 -1].id
-            range(1,oversPlayed*6).forEach(overs => {
-                const team1Nrr = parseFloat((calculateNrr(
-                    team1Runs + runsScored + 1,
-                    convertOversToActualOvers(team1Overs) + (overs/6),
-                    team1AgainstRuns + runsScored,
-                    convertOversToActualOvers(team1AgainstOvers) + convertOversToActualOvers(oversPlayed)
-                )).toFixed(3));
-                const team2Nrr = parseFloat((calculateNrr(
-                    team2Runs + runsScored,
-                    convertOversToActualOvers(team2Overs) + convertOversToActualOvers(oversPlayed),
-                    team2AgainstRuns + runsScored + 1,
-                    convertOversToActualOvers(team2AgainstOvers) + (overs/6)
-                )).toFixed(3));
-                if (
-                    ( team2Position === position  && team1Nrr < aboveTargetNrr && team1Nrr > team2Nrr) ||
-                    (team2Position > position && team1Nrr > targetNrr && team1Nrr < aboveTargetNrr)         ||
-                    (team2Position < position && team1Nrr > targetNrr && team1Nrr < aboveTargetNrr && team1Nrr < team2Nrr)// Check if the NRR is between the target and the one above the target
-                ) {
-                    result.push([overs, team1Nrr, team2Nrr])
-                }
-            })
-            if (result.length === 0) {
-                return `${team1Name} cannot reach position ${position} by the result of this match`;
-            } else {
-                return `${team1Name} need to chase ${runsScored} runs between ${convertRunsToOvers(result[0][0])} and ${convertRunsToOvers(result[result.length -1][0])} overs.\nThe revised NRR of ${team1Name} will be between ${result[result.length -1][1]} and ${result[0][1]}.`
-            }
+    if (tossResult === "Batting First") {
+      const result = [];
+      const team2Position = points[team2 - 1].id;
+      const team1Position = points[team1 - 1].id;
+      // Loop through all the possible runs that can be scored
+      // by the batting team in the given overs played
+      // and calculate the NRR for both teams
+      // NOTE: This can be possibly optimized by two pointer search, but I have not implemented it here.
+      range(1, runsScored - 1).forEach((runs) => {
+        const team1Nrr = parseFloat(
+          calculateNrr(
+            team1Runs + runsScored,
+            team1Overs + oversPlayed,
+            team1AgainstRuns + runs,
+            team1AgainstOvers + oversPlayed,
+          ).toFixed(3),
+        );
+        const team2Nrr = parseFloat(
+          calculateNrr(
+            team2Runs + runs,
+            team2Overs + oversPlayed,
+            team2AgainstRuns + runsScored,
+            team2AgainstOvers + oversPlayed,
+          ).toFixed(3),
+        );
+        if (
+          (team2Position === position &&
+            team1Nrr < aboveTargetNrr &&
+            team1Nrr > team2Nrr) ||
+          (team2Position > position &&
+            team1Nrr > targetNrr &&
+            team1Nrr < aboveTargetNrr) ||
+          (team2Position < position &&
+            team1Nrr > targetNrr &&
+            team1Nrr < aboveTargetNrr &&
+            team1Nrr < team2Nrr) // Check if the NRR is between the target and the one above the target
+        ) {
+          result.push([runs, team1Nrr, team2Nrr]);
         }
+      });
+      if (result.length === 0) {
+        return `${team1Name} cannot reach position ${position} by the result of this match`;
+      } else {
+        return `If ${team1Name} scores ${runsScored} runs in ${oversPlayed} overs, ${team1Name} needs to restrict ${team2Name} between ${result[0][0]} and ${result[result.length - 1][0]} runs in ${oversPlayed} overs. \nThe revised NRR of ${team1Name} will be between ${result[result.length - 1][1]} and ${result[0][1]}.`;
+      }
+    } else if (tossResult === "Bowling First") {
+      const result = [];
+      const team2Position = points[team2 - 1].id;
+      range(1, (oversPlayed - 1) * 6).forEach((balls) => {
+        const team1Nrr = parseFloat(
+          calculateNrr(
+            team1Runs + runsScored + 1,
+            convertOversToActualOvers(team1Overs) + convertBallsToOvers(balls),
+            team1AgainstRuns + runsScored,
+            convertOversToActualOvers(team1AgainstOvers) +
+              convertOversToActualOvers(oversPlayed),
+          ).toFixed(3),
+        );
+        const team2Nrr = parseFloat(
+          calculateNrr(
+            team2Runs + runsScored,
+            convertOversToActualOvers(team2Overs) +
+              convertOversToActualOvers(oversPlayed),
+            team2AgainstRuns + runsScored + 1,
+            convertOversToActualOvers(team2AgainstOvers) +
+              convertBallsToOvers(balls),
+          ).toFixed(3),
+        );
+        // console.log(team1Nrr, team2Nrr, targetNrr, aboveTargetNrr, team2Position, position)
+        if (
+          (team2Position === position &&
+            team1Nrr < aboveTargetNrr &&
+            team1Nrr > team2Nrr) ||
+          (team2Position > position &&
+            team1Nrr > targetNrr &&
+            team1Nrr < aboveTargetNrr) ||
+          (team2Position < position &&
+            team1Nrr > targetNrr &&
+            team1Nrr < aboveTargetNrr
+          ) // Check if the NRR is between the target and the one above the target
+        ) {
+          result.push([balls, team1Nrr, team2Nrr]);
+        }
+      });
+      if (result.length === 0) {
+        return `${team1Name} cannot reach position ${position} by the result of this match`;
+      } else {
+        return `${team1Name} need to chase ${runsScored} runs between ${convertBallsToOvers(result[0][0])} and ${convertBallsToOvers(result[result.length - 1][0])} overs.\nThe revised NRR of ${team1Name} will be between ${result[result.length - 1][1]} and ${result[0][1]}.`;
+      }
     }
-}
+  }
+};
 
 module.exports = {
-    calculatePoints
-}
+  calculatePoints,
+};
